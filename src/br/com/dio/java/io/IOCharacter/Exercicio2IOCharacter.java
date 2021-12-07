8eb03f29a6463076ae6c4cec439d4058e723dbb8
DavidJSantos <davidjames1004@gmail.com>
2021-12-07 19:47:39 -0300

    Resolvendo o exercicio 1

---
+++ src/br/com/dio/java/io/IOCharacter/Exercicio2IOCharacter.java
@@ -0,0 +1,8 @@
+package br.com.dio.java.io.IOCharacter;
+
+//Abra o teclado para escrever 3 filmes que você recomendaria e armazene em "recomendações.txt".
+public class Exercicio2IOCharacter {
+
+
+
+}
