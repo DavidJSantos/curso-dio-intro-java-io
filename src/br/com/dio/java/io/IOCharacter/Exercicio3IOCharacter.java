270634bf3f2444e9ee42d075277f49c5e7ffc9f2
DavidJSantos <davidjames1004@gmail.com>
2021-12-08 18:55:13 -0300

    Resolvendo o exercicio exemplo 3 - parte2

--- src/br/com/dio/java/io/IOCharacter/Exercicio3IOCharacter.java
+++ src/br/com/dio/java/io/IOCharacter/Exercicio3IOCharacter.java
@@ -5,10 +5,7 @@ import java.io.*;
 //Faça uma cópia do arquivo "recomendações.txt" e agora adicione 3 recomendações de livros.
 public class Exercicio3IOCharacter {
 
-    public static void copiarArquivo() throws IOException {
-
-        // Abri o arquivo com metodo "File", recebendo como atributo o pathname do arquivo
-        File f = new File("/home/david/Projeto_Java/curso-dio-intro-java-io/recomendacoes.txt");
+    public static void copiarArquivo(File f) throws IOException {
 
         // Pegando o nome do arquivo e armazenando em uma String
         String nomeArquivo = f.getName();
@@ -37,13 +34,44 @@ public class Exercicio3IOCharacter {
             linha = br.readLine();
         }while (!(linha == null));
         System.out.printf("Arquivo \"%s\" copiado com sucesso!%n", nomeArquivo);
-        System.out.printf("Arquivo \"%s\" criado com sucesso! (%d bytes)", nmArquivoCopia, fCopy.length());
+        System.out.printf("Arquivo \"%s\" criado com sucesso! (%d bytes)%n", nmArquivoCopia, fCopy.length());
+
+        PrintWriter pw = new PrintWriter(System.out);
+        pw.println("Recomende 3 livros: ");
+        pw.flush();
+
+        addInfoArquivo(fCopy.getName());
+
+        pw.printf("Linhas adicionadas ao arquivo \"%s\" com sucesso! (%d bytes)", nmArquivoCopia, fCopy.length());
+
+        br.close();
+        bw.close();
+        pw.close();
     }
-    public static void AddInfoArquivo(){
+    public static void addInfoArquivo(String arquivo) throws IOException {
+
+        // Preparando a entrada das informações com o metodo "BufferedReader"
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // Abrindo o prompt para receber a entrada
+        String line = br.readLine();
+        // Preparando o arquivo para receber a entrada. (O atributo append: "true" informa que a linha deve ser adicionada sem sobrescrever o arquivo).
+        BufferedWriter bw = new BufferedWriter(new FileWriter(arquivo, true));
+
+        // Escrevendo no arquivo, linha a linha
+        do {
+            bw.write(line);
+            bw.newLine();
+            bw.flush();
+            line = br.readLine();
+        }while (!(line.equalsIgnoreCase("fim")));
 
     }
 
     public static void main(String[] args) throws IOException {
-        copiarArquivo();
+
+        // Abrindo o arquivo com metodo "File", recebendo como atributo o pathname do arquivo
+        File f = new File("/home/david/Projeto_Java/curso-dio-intro-java-io/recomendacoes.txt");
+        copiarArquivo(f);
+
     }
 }
